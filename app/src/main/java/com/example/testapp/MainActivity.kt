486149c81f576b56cf42c2d947b18529b122d395
nimra ajmal <nimra.ajmal@and.digital>
2021-08-25 14:05:26 +0100

    step 2 of adding Auth

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testapp
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,7 +10,13 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import com.amplifyframework.AmplifyException
+import com.amplifyframework.auth.AuthUserAttributeKey
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin
+import com.amplifyframework.auth.options.AuthSignUpOptions
+import com.amplifyframework.core.Amplify
 import com.example.testapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +30,40 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        try { // Add this line, to include the Auth plugin.
+            Amplify.configure(applicationContext)
+            Amplify.addPlugin(AWSCognitoAuthPlugin())
+            println("Initialized Amplify")
+
+            Amplify.Auth.fetchAuthSession(
+                { Log.i("AmplifyQuickstart", "Auth session = $it") },
+                { Log.e("AmplifyQuickstart", "Failed to fetch auth session" ) }
+            )
+
+            val options = AuthSignUpOptions.builder()
+                .userAttribute(AuthUserAttributeKey.email(), "my@email.com")
+                .build()
+            Amplify.Auth.signUp("username", "Password123", options,
+                { Log.i("AuthQuickStart", "Sign up succeeded: $it") },
+                { Log.e ("AuthQuickStart", "Sign up failed", it) }
+            )
+
+            Amplify.Auth.confirmSignUp(
+                "username", "the code you received via email",
+                { result ->
+                    if (result.isSignUpComplete) {
+                        Log.i("AuthQuickstart", "Confirm signUp succeeded")
+                    } else {
+                        Log.i("AuthQuickstart","Confirm sign up not complete")
+                    }
+                },
+                { Log.e("AuthQuickstart", "Failed to confirm sign up", it) }
+            )
+
+        } catch (error: AmplifyException) {
+            println(error)
+        }
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
